2eaf743c4844eaeed929cb09f7c80a7bf33ef38b
Sergey Yavnyi <blacktaxi@gmail.com>
2016-01-23 18:03:56 -0800

    Improve test infrastructure

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-elm-package install -y
-console_version=$(node -pe 'JSON.parse(process.argv[1])["laszlopandy/elm-console"]' "$(cat elm-stuff/exact-dependencies.json)")
-
-elm-make --yes --output build/raw-test.js tests/Tests.elm
-bash elm-stuff/packages/laszlopandy/elm-console/$console_version/elm-io.sh build/raw-test.js build/test.js
-rm build/raw-test.js
-node build/test.js
-rm build/test.js
